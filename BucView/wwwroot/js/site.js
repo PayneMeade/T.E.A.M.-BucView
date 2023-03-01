2b5b35c8b5a979f1efd5372437d957241c357230
Chase <Chase@LAPTOP>
2023-03-01 16:46:39 -0500

    Remove testing js

--- BucView/wwwroot/js/site.js
+++ BucView/wwwroot/js/site.js
@@ -1,11 +1,1 @@
-﻿"using strict"
-
-import { getNext } from "./TourAPI.js";
-
-var button = document.getElementById("tourButton");
-button.addEventListener("click", function(e){
-    var result1 = getNext();
-    console.log(result1);
-})
-
-
+﻿
