7d4ff67706f54092d8a12c931093182e9db6dfae
ltwilson4132 <wilsonlt@etsu.edu>
2023-03-22 15:18:20 -0400

    Cookie controller added

---
+++ BucView/Controllers/CookieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BucView.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!HttpContext.Request.Cookies.ContainsKey("page"))
+            {
+                HttpContext.Response.Cookies.Append("page", "WebPage");
+                return View();
+            }
+            else
+            {
+                var page = HttpContext.Request.Cookies["page"];
+                return View(page);
+            }
+        }
+    }
+}
