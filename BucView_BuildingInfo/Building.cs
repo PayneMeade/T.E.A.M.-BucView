eaa1653e125881b4e64d227da525c0dac6ddb0e5
ltwilson4132 <wilsonlt@etsu.edu>
2023-04-11 19:40:44 -0400

    Added new project for JSON Microservice
    Titled BucView_BuildingInfo

---
+++ BucView_BuildingInfo/Building.cs
@@ -0,0 +1,10 @@
+namespace BucView_BuildingInfo
+{
+    public class Building
+    {
+        public string? buildingName { get; set; }
+        public string? buildingType { get; set; }
+        public string? directionsTo { get; set; }
+        public buildingInfo? buildingInfo { get; set; }
+    }
+}
---
+++ BucView_BuildingInfo/buildingInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BucView_BuildingInfo
+{
+    public class buildingInfo
+    {
+        public string? bio { get; set; }
+        public List<string>? tags { get; set; }
+        public string? Long { get; set; }
+        public string? Lat { get; set; }
+    }
+}
