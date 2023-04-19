16a464ea24325ddd526c906d0f63c89853798f7a
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2023-04-19 15:59:56 -0400

    When the user gets close to a building, the tour automates to the next page

--- BucView/wwwroot/js/site.js
+++ BucView/wwwroot/js/site.js
@@ -3,15 +3,45 @@
 
 // Write your JavaScript code.
 
-
-
-let lat = document.getElementById('toLat').value;
-let long = document.getElementById('toLong').value;
+let lat = parseFloat(document.getElementById('toLat').value);
+let long = parseFloat(document.getElementById('toLong').value);
+let toBuilding = document.getElementById('toBuilding').value;
 console.log("Lat", lat);
 console.log("Long", long);
 
+//this function uses the Haversine formula
+//this formula calculates the shortest distance between two points on the Earth
+//(or a sphere in general)
+//https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128
+const distanceToBuilding = (userLat, userLong, buildingLat, buildingLong) => {
+    const R = 6.371e6 //the Earth's mean radius in meters
+
+    //convert to radians
+    let phi1 = userLat * Math.PI / 180;
+    let phi2 = buildingLat * Math.PI / 180;
+
+    //difference in longitude and latitude in radians
+    let delta_phi = (buildingLat - userLat) * Math.PI / 180;
+    let delta_lambda = (buildingLong - userLong) * Math.PI / 180;
+
+    //haversine formula
+    let a = Math.pow(Math.sin(delta_phi / 2), 2)
+        + Math.cos(phi1) * Math.cos(phi2)
+        * Math.pow(Math.sin(delta_lambda / 2), 2);
+
+    //central angle (angle between the two points relative to the Earth's center) in radians
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    let d = R * c; //distance in meters (arc length of the central angle)
+
+    return d;
+}
+
 const positionCallback = (position) => {
-    console.log(position);
+    let userLat = position.coords.latitude;
+    let userLong = position.coords.longitude;
+    if (distanceToBuilding(userLat, userLong, lat, long) <= 15)
+        window.location.href = encodeURI(`../../Tour/Index/${toBuilding}`);
 }
 
 const errorCallback = (error) => {
@@ -19,7 +49,7 @@ const errorCallback = (error) => {
 }
 
 const getLocation = () => {
-    navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(positionCallback, errorCallback, { enableHighAccuracy: true });
 }
 
 var map = L.map('map', {
@@ -42,4 +72,4 @@ var marker = L.marker([lat, long]);
 // Adding marker to the map
 marker.addTo(map);
 
-//setInterval(getLocation, 1000);
+getLocation();
